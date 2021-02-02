a0d628522d11e45bb7c426925bd99ade1bc52da5
Leonid Vasilev <savidiy@mail.ru>
2021-02-02 19:12:25 +0300

    add mines

---
+++ Assets/Scripts/Utility/TimerActivator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimerActivator : MonoBehaviour
+{
+    [SerializeField] float _delay;
+    [SerializeField] UnityEvent _onTime;
+    float _timer = 0;
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= _delay)
+        {
+            _onTime.Invoke();
+            Destroy(this);
+        }
+    }
+}
