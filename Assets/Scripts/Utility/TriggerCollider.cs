48bd43997ac4aa445a4cc8d6710ee30f0bf0fc2e
Leonid Vasilev <savidiy@mail.ru>
2021-02-05 12:18:53 +0300

    clear triiger colleder script

--- Assets/Scripts/Utility/TriggerCollider.cs
+++ Assets/Scripts/Utility/TriggerCollider.cs
@@ -6,9 +6,19 @@ using UnityEngine.Events;
 public class TriggerCollider : MonoBehaviour
 {
     [SerializeField] UnityEvent _onEnter;
+    [SerializeField] bool _mustCheckTag = true;
+    [SerializeField] string _checkedTag = "Player";
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (_mustCheckTag)
+        {
+            if (other.gameObject.CompareTag(_checkedTag))
+            {
+                _onEnter.Invoke();
+            }   
+        }
+        else
         {
             _onEnter.Invoke();
         }
