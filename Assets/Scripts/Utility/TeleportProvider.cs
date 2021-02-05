5d9b072fbbe79027771e6c9a65e62d93c68825c6
Leonid Vasilev <savidiy@mail.ru>
2021-02-05 13:55:48 +0300

    add teleport to start player

---
+++ Assets/Scripts/Utility/TeleportProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportProvider : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+    public void TeleportToPoint(Transform point)
+    {
+        _target.position = point.position;
+    }
+}
